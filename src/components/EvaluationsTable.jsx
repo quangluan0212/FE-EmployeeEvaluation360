c738c387b88fd7d906b85c6749fb0457e17814a5
Le Quang Luan <quangluan0212@gmail.com>
2025-06-29 00:57:07 +0700

    fix/table dashboard

--- src/components/EvaluationsTable.jsx
+++ src/components/EvaluationsTable.jsx
@@ -168,9 +168,9 @@ const EvaluationsTable = ({
                     <div className="flex items-center">
                       <span
                         className={`inline-flex items-center px-3 py-1 rounded-full text-sm font-semibold ${
-                          item.diemTongKet >= 8
+                          item.diemTongKet >= 90
                             ? "bg-green-100 text-green-800"
-                            : item.diemTongKet >= 6
+                            : item.diemTongKet >= 40
                             ? "bg-yellow-100 text-yellow-800"
                             : "bg-red-100 text-red-800"
                         }`}
